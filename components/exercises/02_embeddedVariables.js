42d25d0d19ff4ab9a922e8c4d84720b1ee5bd02a
Jiayu Fan <Klein2339@gmail.com>
2020-10-11 20:24:59 +0100

    Exercise 2 completed

--- components/exercises/02_embeddedVariables.js
+++ components/exercises/02_embeddedVariables.js
@@ -1,10 +1,55 @@
 import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
 const Demo = () => {
+  const course = "Software Systems Practice - Modules Table";
+  const modules = [
+    {
+      name: "Agile Software",
+      noLectures: 3,
+      noPracticals: 2
+    },
+    {
+      name: "Web App Development",
+      noLectures: 1,
+      noPracticals: 4
+    },
+    {
+      name: "Enterprise Systems Architechure",
+      noLectures: 5,
+      noPracticals: 8
+    }
+  ];
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>{course}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{modules[0].name}</td>
+            <td>{modules[0].noLectures}</td>
+            <td>{modules[0].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].name}</td>
+            <td>{modules[1].noLectures}</td>
+            <td>{modules[1].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{modules[2].name}</td>
+            <td>{modules[2].noLectures}</td>
+            <td>{modules[2].noPracticals}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
