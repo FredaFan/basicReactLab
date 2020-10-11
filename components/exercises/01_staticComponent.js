67ea2bd8ca0f7074f276278e7747c51d9d351896
Jiayu Fan <Klein2339@gmail.com>
2020-10-11 19:57:28 +0100

    Exercise 1 completed

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -3,7 +3,40 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>Software Systems Practice - Modules Table</h1>
+      <table className="table table-bordered">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>No lectures</th>
+      <th>No practicals</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr>
+      <td>Agile Software</td>
+      <td>2</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Web App Development</td>
+      <td>3</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Buiness Analytics</td>
+      <td>1</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>Enterprise Systems Architechure</td>
+      <td>5</td>
+      <td>8</td>
+    </tr>
+  </tbody >
+</table>
+</div>
   );
 };
 
